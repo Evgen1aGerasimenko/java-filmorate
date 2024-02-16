a31b7c3e14fb569767c7fd807e1143fdc44d305b
Evgenia <evgen1agerasimenko@yandex.ru>
2024-02-16 22:56:16 +0300

    # Спринт 9

--- src/main/java/ru/yandex/practicum/filmorate/model/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/Film.java
@@ -1,12 +1,25 @@
 package ru.yandex.practicum.filmorate.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import ru.yandex.practicum.filmorate.validators.ReleaseDateConstraint;
+
+import javax.validation.constraints.*;
+import java.time.LocalDate;
 
 /**
  * Film.
  */
-@Getter
-@Setter
+@Data
+@NoArgsConstructor
 public class Film {
+    private int id;
+    @NotBlank
+    private String name;
+    @Size(min = 0, max = 200)
+    private String description;
+    @ReleaseDateConstraint
+    private LocalDate releaseDate;
+    @Min(1)
+    private int duration;
 }
---
+++ src/main/java/ru/yandex/practicum/filmorate/validators/ReleaseDateConstraint.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.validators;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Documented
+    @Constraint(validatedBy = ReleaseDataValidator.class)
+    @Target({ ElementType.METHOD, ElementType.FIELD })
+    @Retention(RetentionPolicy.RUNTIME)
+    public @interface ReleaseDateConstraint {
+        String message() default "Film release data must be after 28.12.1895";
+        Class<?>[] groups() default {};
+        Class<? extends Payload>[] payload() default {};
+
+}
